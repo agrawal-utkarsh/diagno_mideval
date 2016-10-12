2baea86334745789fc6f96e9eba6f2252c6a45f8
wompowe <utkarsh.agrawal@rediff.com>
2016-10-12 11:27:54 +0530

    asdf

--- app/src/main/java/com/example/online/MainActivity.java
+++ app/src/main/java/com/example/online/MainActivity.java
@@ -5,12 +5,25 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class MainActivity extends Activity {
 
@@ -42,6 +55,103 @@ public class MainActivity extends Activity {
         e=(EditText)findViewById(R.id.uiop);
 
     }
+
+
+
+
+
+
+
+
+
+
+    public void onLogin(View view)
+    {
+            text=e.getText().toString();
+
+        new BTask().execute(text);
+
+
+        }
+
+    class BTask extends AsyncTask<String, Void, String> {
+        String json_url;
+
+        @Override
+        protected void onPreExecute() {
+            json_url = "http://lapla.net23.net/login.php";
+        }
+
+
+        @Override
+        protected String doInBackground(String... args) {
+            String check;
+            check = args[0];
+            try {
+                URL url = new URL(json_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String data = URLEncoder.encode("login_name", "UTF-8") + "=" + URLEncoder.encode(check, "UTF-8");
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                String response = "";
+                String line = "";
+                while ((line = bufferedReader.readLine()) != null) {
+                    response += line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return response;
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public void addApp(View view)
     {
